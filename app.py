626df2083e5afa6626e8bcb30ca46e94314efb92
localhost-server <99524394+localhost-server@users.noreply.github.com>
2025-03-16 17:22:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Title and description
-st.markdown("## 🎨 Artist QR Code Generator", help=None)
+st.markdown("# 🎨 Artist QR Code Generator", help=None)
 
 from utils.qr_scanner import QRScanner
 from utils.ui_components import ScannerUI, SettingsUI, QRDisplayUI
